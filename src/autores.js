a90da7b2eb24044265c1bd3a64d13276f4aef2fa
Isabella <isabella.soares.28@gmail.com>
2020-03-02 20:27:41 -0300

    feat: data mock

---
+++ src/autores.js
@@ -0,0 +1,22 @@
+export const AUTORES = [
+    {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+    },
+    {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+    },
+    {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+    },
+    {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+    }
+];
