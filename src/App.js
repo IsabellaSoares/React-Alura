d27ad30f40e6d3bc577d960320f9a15e284cc4b3
Isabella <isabella.soares.28@gmail.com>
2020-03-02 20:28:23 -0300

    feat: transform to Hooks

--- src/App.js
+++ src/App.js
@@ -1,57 +1,31 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import Table from './Table';
 import Form from './Formulario';
 import Header from './Header';
 import './App.css';
+import { AUTORES } from './autores';
 
-class App extends Component {
-	state = {
-		autores: [
-			{
-				nome: 'Paulo',
-				livro: 'React',
-				preco: '1000'
-			},
-			{
-				nome: 'Daniel',
-				livro: 'Java',
-				preco: '99'
-			},
-			{
-				nome: 'Marcos',
-				livro: 'Design',
-				preco: '150'
-			},
-			{
-				nome: 'Bruno',
-				livro: 'DevOps',
-				preco: '100'
-			}
-		]
-  };
-  
-  removeAutor = (index) => {
-    this.setState({
-      autores: this.state.autores.filter((autor, posicaoAtual) => index !== posicaoAtual)
-    });
-  }
+function App() {
+	const [ autores, setAutores ] = useState(AUTORES);
 
-  escutadorDeSubmit = autor => {
-	  this.setState({ autores: [...this.state.autores, autor] });
-  }
+	function removeAutor(index) {
+		setAutores(autores.filter((autor, posicaoAtual) => index !== posicaoAtual));
+	};
 
-	render() {
-		return (
-			<Fragment>
-				<Header />
-				<div className="container mb-10">
-					<Table autores={this.state.autores} removeAutor={this.removeAutor}/>
-					<Form escutadorDeSubmit={this.escutadorDeSubmit} />
-				</div>
-			</Fragment>
-		);
-	}
+	function escutadorDeSubmit(autor) {
+		setAutores([ ...autores, autor ]);
+	};
+
+	return (
+		<Fragment>
+			<Header />
+			<div className="container mb-10">
+				<Table autores={autores} removeAutor={removeAutor} />
+				<Form escutadorDeSubmit={escutadorDeSubmit} />
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
