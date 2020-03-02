70e7c5986a81b2b35ec54e3de13e8103e1d8a715
Isabella <isabella.soares.28@gmail.com>
2020-03-02 20:46:17 -0300

    refactor: transform to Hooks

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,75 +1,78 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
-export default class Formulario extends Component {
-	constructor(props) {
-		super(props);
+export default function Formulario(props) {
+	const [ filterInput, setFilterInput ] = useReducer((state, newState) => ({ ...state, ...newState }), {
+		nome: '',
+		livro: '',
+		preco: ''
+	});
 
-		this.stateInicial = {
+	function escutadorDeInput(event) {
+		const { name, value } = event.target;
+		setFilterInput({ [name]: value });
+	}
+
+	function submitForm() {
+		props.escutadorDeSubmit(filterInput);
+		setFilterInput({
 			nome: '',
 			livro: '',
 			preco: ''
-		};
-
-		this.state = this.stateInicial;
-	}
-
-	escutadorDeInput = (event) => {
-		const { name, value } = event.target;
-
-		this.setState({
-			[name]: value
 		});
-	};
-
-	submitForm = () => {
-		this.props.escutadorDeSubmit(this.state);
-		this.setState(this.stateInicial);
-	};
-
-	render() {
-		const { nome, livro, preco } = this.state;
-
-		return (
-			<form className="container centered">
-				<div className="row">
-					<div className="input-field col s4">
-						<label htmlFor="nome" className="input-field">Nome</label>
-						<input 
-							id="nome"
-							type="text"
-							name="nome"
-							value={nome}
-							onChange={this.escutadorDeInput}
-							className="validate" />
-					</div>
-
-					<div className="input-field col s4">
-						<label htmlFor="livro" className="input-field">Livro</label>
-						<input
-							id="livro"
-							type="text"
-							name="livro"
-							value={livro}
-							onChange={this.escutadorDeInput}
-							className="validate" />
-					</div>
-
-					<div className="input-field col s4">
-						<label htmlFor="preco" className="input-field">Preço</label>
-						<input
-							id="preco"
-							type="text"
-							name="preco"
-							value={preco}
-							onChange={this.escutadorDeInput}
-							className="validate" />
-					</div>
-				</div>
-				<button type="button" className="waves-effect waves-light btn indigo lighten-2" onClick={this.submitForm}>
-					Salvar
-				</button>
-			</form>
-		);
 	}
+
+	return (
+		<form className="container centered">
+			<div className="row">
+				<div className="input-field col s4">
+					<label htmlFor="nome" className="input-field">
+						Nome
+					</label>
+					<input
+						id="nome"
+						type="text"
+						name="nome"
+						value={filterInput.nome}
+						onChange={escutadorDeInput}
+						className="validate"
+					/>
+				</div>
+
+				<div className="input-field col s4">
+					<label htmlFor="livro" className="input-field">
+						Livro
+					</label>
+					<input
+						id="livro"
+						type="text"
+						name="livro"
+						value={filterInput.livro}
+						onChange={escutadorDeInput}
+						className="validate"
+					/>
+				</div>
+
+				<div className="input-field col s4">
+					<label htmlFor="preco" className="input-field">
+						Preço
+					</label>
+					<input
+						id="preco"
+						type="text"
+						name="preco"
+						value={filterInput.preco}
+						onChange={escutadorDeInput}
+						className="validate"
+					/>
+				</div>
+			</div>
+			<button
+				type="button"
+				className="waves-effect waves-light btn indigo lighten-2"
+				onClick={() => submitForm()} >
+				Salvar
+			</button>
+		</form>
+	);
 }
